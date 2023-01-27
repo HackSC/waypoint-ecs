b79bc64cf9ce33e615b1c23fb5f350e720d83674
Vincent <vinceny.fe@gmail.com>
2023-01-27 12:38:54 -0800

    udpate

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(rw, "Hello World v7")
+		fmt.Fprint(rw, "Hello World v7; this is an update")
 	})
 
 	http.ListenAndServe(":3000", nil)
